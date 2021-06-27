201aaebb7556786cbefd937bcaf97f1010e1e8ce
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-06-27 13:12:06 +0900

    [feat]型注釈と型推論の説明

---
+++ index.ts
@@ -0,0 +1,16 @@
+let hasValue: boolean = true;
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
+
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'Jack',
+    age: 21
+}
+console.log(person.gender)
