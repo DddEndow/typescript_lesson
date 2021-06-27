7766f35aea3203870b91b63e3779c5189e3d3575
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-06-27 19:28:15 +0900

    [feat]LookUp型でオブジェクトのメンバーの型を取得

--- advanced.ts
+++ advanced.ts
@@ -101,3 +101,4 @@ const downloadedData: DownloadedData = {
 }
 console.log(downloadedData.user?.name?.first);
 const userData = downloadedData.user ?? 'no-user'
+type id = DownloadedData["id" | "user"]
