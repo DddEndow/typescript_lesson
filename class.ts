4356ee7e8df1cf09a0add929e195c5bc607e32f5
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-06-27 16:46:39 +0900

    [feat]getter, setterを追加

--- class.ts
+++ class.ts
@@ -12,7 +12,19 @@ class Person {
 }
 
 class Teacher extends Person {
-    constructor(name: string, age: number, public subject: string) {
+    get subject() {
+        if (!this._subject) {
+            throw new Error('There is no subject');
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        if (!value) {
+            throw new Error('There is no subject');
+        }
+        this._subject = value;
+    }
+    constructor(name: string, age: number, private _subject: string) {
         super(name, age);
     }
 
@@ -22,4 +34,6 @@ class Teacher extends Person {
 }
 
 const teacher = new Teacher('Quill', 38, 'Math');
+teacher.subject = 'Music';
+console.log(teacher.subject);
 teacher.greeting();
