669408d056273201b2ba424005d5c7704df1a8c9
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-06-28 22:56:49 +0900

    [feat]メソッドデコレータとアクセサーデコレータ

--- decorator.ts
+++ decorator.ts
@@ -30,13 +30,38 @@ function PropertyLogging(target: any, propertyKey: string) {
     console.log(propertyKey);
 }
 
+function MethodLogging(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log('MethodLogging');
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+}
+
+function AccessorLogging(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log('AccessorLogging');
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+}
+
 @Logging('Logging User')
 @Component('<h1>{{ name }}</h1>', '#app') // 下から実行される
 class User {
     @PropertyLogging
     name = 'Quill';
-    constructor(public age: number) {
+    constructor(private _age: number) {
         console.log('User was created!');
+    }
+    @AccessorLogging
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        this._age = value;
+    }
+    @MethodLogging
+    greeting() {
+        console.log('Hello!');
     }
 }
 const user1 = new User(32);
