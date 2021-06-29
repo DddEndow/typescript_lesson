f53b0f9edec7ccdc90275137ded38880c2885a31
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-06-29 22:52:30 +0900

    [feat]reactでの型推論

--- react/typescript-react/src/App.tsx
+++ react/typescript-react/src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Hello from "./components/Hello";
 
 function App() {
-  return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-  );
+    const [name, setName] = useState<string | null>('Peter');
+    return (
+        <div>
+            <Hello message={`I am ${name}!`}></Hello>
+        </div>
+    );
 }
 
 export default App;
