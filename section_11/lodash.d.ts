d9fb0b0a204af4a774348b27b80141ab3b0063de
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-06-29 17:55:47 +0900

    [feat]lodashの型ファイルを作る

--- section_11/lodash.d.ts
+++ section_11/lodash.d.ts
@@ -1,3 +1,9 @@
-declare module 'lodash' {
-    export function shuffle(arr: T[]): T[]
-}
+// declare module 'lodash' {
+//     export function shuffle(arr: T[]): T[]
+// }
+// interface Lodash {
+//     shuffle(arr: T[]): T[]
+// }
+// declare namespace _ {
+//     function shuffle(arr: T[]): T[]
+// }
